c3a413f6980c0d3f8171efb99c7f5b97cd3d289e
kalyan <pvkalyan427@gmail.com>
2020-01-27 21:43:13 -0800

    Method1

---
+++ if else statements/Sum and Average method1.playground/Contents.swift
@@ -0,0 +1,37 @@
+/* Write a program in swift to input 5 numbers from keyboard and find their sum and average. Go to the editor
+
+Test Data
+Input the 5 numbers : 1 2 3 4 5
+Expected Output :
+
+Input the 5 numbers :
+1
+2
+3
+4
+5
+The sum of 5 no is : 15
+The Average is : 3.0  */
+
+
+
+
+import UIKit
+
+var value1: Double = 100.5
+var value2: Double = 200.7
+var value3: Double = 300.9
+var value4: Double = 400.10
+var value5: Double = 500.123
+let sum: Double
+let average: Double
+
+sum = value1+value2+value3+value4+value5
+print("Sum value is \(sum)")
+
+average = (value1+value2+value3+value4+value5)/5
+print("Average value is \(average)")
+
+
+
+
