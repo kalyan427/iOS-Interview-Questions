62d89e5026316d78f5649f60d819a33a02a4bacf
kalyan <pvkalyan427@gmail.com>
2020-01-27 21:20:53 -0800

    Greatest of three numbers method1

---
+++ if else statements/Greatest of three numbers.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var value1: Int = 25
+var value2: Int = 78
+var value3: Int = 87
+
+if (value1 > value2 && value1 > value3) {
+    print("value1 is greater")
+} else if (value2 > value1 && value2 > value3) {
+    print("value2 is greater")
+} else {
+    print("value3 is greater")
+}
+
+
